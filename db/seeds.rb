ffbf351f8950c02d8998cfdb310f4ea93c91c015
Rick G <richkws@gmail.com>
2018-03-09 13:22:49 +0000

    Install Faker and seed DB with users and items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+# Create 10 users
+10.times do
+  name = Faker::Name.name
+  first = name.split[0]
+  email = Faker::Internet.email(first)
+  password = Faker::Number.number(10)
+  User.create!(name: name, email: email, password: password )
+end
+
+users = User.all
+
+# Create a couple of known users for testing views
+rick = User.create!(name: 'Rick', email: 'rick@example.com', password: 'abcdef')
+jack = User.create!(name: 'Jack', email: 'jack@example.com', password: 'abcdef')
+
+# Create 50 items, each with a random user
+50.times do
+  name = Faker::Company.bs
+  user = users.sample
+  Item.create!(name: name, user: user )
+end
+
+puts "#{User.count} users were created"
+puts "#{Item.count} items were created"
+
