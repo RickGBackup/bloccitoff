b516fce3b04a584a7a79b7435ae56010eb2ea350
Rick G <richkws@gmail.com>
2018-03-09 15:52:48 +0000

    Implement items destroy action and AJAXify delete links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,26 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.build(item_params)
     
-   if @item.save
+    if @item.save
       flash[:success] = "Item saved!"
       redirect_to current_user
-   else
+    else
       flash[:alert] = "Item could not be saved."
       redirect_to :back
-   end
+    end
+  end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    
+    if @item.user == current_user
+      @item.destroy
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js 
+    end
   end
 
   private
