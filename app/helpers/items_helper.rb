d0b70dda236942efa9cddcc31f20a18d378b4b2e
Rick G <richkws@gmail.com>
2018-03-10 21:27:31 +0000

    Add time remaining to items in users' show

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,6 @@
 module ItemsHelper
+  def time_remaining(item)
+    # return time diff between now and 7 days from when item was created.
+    distance_of_time_in_words(item.created_at + (60*60*24*7), Time.now )+" left"
+  end
 end
